ef874d5706ff5f62a706a8dda0da88fa2bc29cb2
Hamza <hamzazaidi253@gmail.com>
2026-02-03 10:21:46 +0100

    ok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
-package org.example;
-
-import service.TradingPlatform;
+//package org.example;
+//
+//import service.TradingPlatform;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        TradingPlatform t = new TradingPlatform();
-        t.appInerface();
-    }
+//public class Main {
+//    static void main() {
+//        TradingPlatform t = new TradingPlatform();
+//        t.appInerface();
+//    }
 }
